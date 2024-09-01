7e24a6b84274a7e50ebb5e89f23308d41376e0ed
lee <Lz1958455046@outlook.com>
2024-09-01 09:51:10 +0800

    fix: 修正返回时间

--- global/base_model.go
+++ global/base_model.go
@@ -5,7 +5,7 @@ import "time"
 // BaseModel Base model struct, not including deleted_at field
 type BaseModel struct {
 	ID        uint       `gorm:"primarykey" json:"ID"` // Primary key ID
-	CreatedAt time.Time  // Creation time
-	UpdatedAt time.Time  // Update time
-	DeleteAt  *time.Time `gorm:"index" json:"-"` // Delete time, not included in JSON
+	CreatedAt time.Time  `json:"createdAt"`            // Create time
+	UpdatedAt time.Time  `json:"updatedAt"`            // Update time
+	DeleteAt  *time.Time `gorm:"index" json:"-"`       // Delete time, not included in JSON
 }
